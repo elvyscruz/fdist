567e10decd073262b50712b04f9f8ec8524a6a1a
elvyscruz <elvys.cruz@gmail.com>
2017-08-08 01:12:47 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var exports = module.exports = {}
+
+exports.freqDist = function (arr) {
+
+if(Array.isArray(arr)){
+
+  var index = 0
+  var dist = {}
+
+  arr.map(function (i) {
+    return arr.filter(function (f) {
+        return i === f
+    }).length
+  }).forEach(function (freq) {
+    dist[arr[index]] = freq
+    ++index;
+  })
+
+} else return new TypeError()
+
+return dist;
+}
